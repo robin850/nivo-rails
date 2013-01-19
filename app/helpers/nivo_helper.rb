24873633326b816b54292bafb886796c1bb480c9
Robin Dupret <robin.dupret@gmail.com>
2013-01-19 18:09:23 +0100

    Fix html code generation to be prettier

--- app/helpers/nivo_helper.rb
+++ app/helpers/nivo_helper.rb
@@ -1,12 +1,12 @@
 module NivoHelper
   def nivo_slider(hash = {}, &block)
-    options = { :theme => :default, :id => "slider" }
+    options = { :theme => :default, :id => "" }
     options.merge!(hash)
     klass = "slide-wrapper theme-#{options[:theme]}"
     id = options[:id]
 
-    content_tag(:div, :id => id, :class => klass) do
-      content_tag(:div, :class => "nivoSlider #{options[:class]}") do
+    content_tag(:div, :class => klass) do
+      content_tag(:div, :id => id, :class => "nivoSlider #{options[:class]}") do
         yield
       end
     end
